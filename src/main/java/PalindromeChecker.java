63342e94d6b5d2b2a68282cbb5747f8ff12c6d82
dialtamirano <143028005+dialtamirano@users.noreply.github.com>
2024-02-02 20:23:24 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -1,7 +1,30 @@
-public void setup(){
-  String lines[] = loadStrings("palindromes.txt");
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class PalindromeChecker {
+public void tester()
+{
+  //String lines[] = loadStrings("palindromes.txt");
+  String[] lines = new String[6];
+    try{
+        File myFile = new File("palindromes.txt");
+        Scanner myReader = new Scanner(myFile);
+        int counter = 0;
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            lines[counter] = data;
+            counter++;
+        }
+        myReader.close();
+    }
+    catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
   System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++){
+  for (int i=0; i < lines.length; i++)
+  {
     if(palindrome(lines[i])==true)
     {
       System.out.println(lines[i] + " IS a palindrome.");
@@ -12,36 +35,34 @@ public void setup(){
     }
   }
 }
-
-public boolean palindrome(String word){
-  //your code here
-  String backwards = onlyLetters(word.toLowerCase());
-  if (backwards.equals(reverse(backwards))){
+public boolean palindrome(String word)
+{
+String backwards = onlyLetters(word.toLowerCase());
+   
+  if (backwards.equals(reverse(backwards))) {
     return true;
-  }
+  }  
+  //your code here
   return false;
 }
-
 public String reverse(String str)
 {
     String sNew = new String();
-
- 
- 
-  for (int i = str.length()-1; i >= 0; i-- ) {
+ for (int i = str.length()-1; i >= 0; i-- ) {
     sNew += str.substring(i,i+1);
   }
- 
+   
+    //your code here
     return sNew;
 }
 
-public String onlyLetters(String sString)
-{
+  public String onlyLetters(String sString){
   String s = "";
   for ( int i = 0; i < sString.length(); i++) {
     if (Character.isLetter(sString.charAt(i))) {
        s += sString.charAt(i);
     }
   }
- return s;
+  return s;
+}
 }
